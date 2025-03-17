b39c24da57de1742eddda3b7d58c6b904ac130c5
PorkMaster73 <66413622+PorkMaster73@users.noreply.github.com>
2025-03-17 13:44:31 -0700

    Update updater.py
    Integrated the includes.txt functionality into the updater.

--- updater.py
+++ updater.py
@@ -3,31 +3,43 @@ import requests
 import subprocess
 
 # // GitHub URL of the raw version of xbox360wrapper_main.py
-url = "https://raw.githubusercontent.com/PorkMaster73/x360-visual-wrapper/main/xbox360wrapper_main.py"
+url = "https://raw.githubusercontent.com/PorkMaster73/x360-visual-wrapper/main/"
+includes_fname =  f"includes.txt"
 
-# // Name of the local file to replace
-file_name = "xbox360wrapper_main.py"
+includes = download_write_to_file(includes_fname, False)
+includes = [x.strip() for x in includes.split("\n") if (x and "---" not in x)]
+includes = [x[5:] for x in includes]  # // Gets rid of the "root/" prefix.
 
-# // Check if the file exists before downloading
-if os.path.exists(file_name):
-    print(f"Found existing {file_name}, preparing to update.")
 
-# // Download the latest xbox360wrapper_main.py
-try:
-    print(f"Downloading the latest {file_name} from GitHub...")
-    response = requests.get(url)
-    response.raise_for_status()  # Check if the request was successful
-    with open(file_name, "wb") as file:
-        file.write(response.content)
-    print(f"Successfully downloaded and replaced {file_name}.")
-except requests.exceptions.RequestException as e:
-    print(f"Failed to download the file: {e}")
-    exit(1)
+def download_write_to_file(file_path: str, save_locally=True) -> str:
+    # // Download the latest xbox360wrapper_main.py
+    try:
+        print(f"Downloading the latest {file_path} from GitHub...")
+        response = requests.get(url + filepath)
+        response.raise_for_status()  # // Check if the request was successful
+        if (save_locally):
+            with open(file_path, "wb") as file:
+                file.write(response.content)
+            print(f"Successfully downloaded and replaced {file_path}.")
+        return response.content
+    except requests.exceptions.RequestException as e:
+        print(f"Failed to download the file: {e}")
+        exit(1)  # // Returns nothing.
+
+
+# // Loop through each file to be included.
+for file_name in includes:
+    # // Check if the file exists before downloading   
+    if os.path.exists(file_name):
+        print(f"Found existing {file_name}, preparing to update.")
+    download_write_to_file(file_name, True)
+
 
 # // Run the updated xbox360wrapper_main.py
+run_file = "xbox360wrapper_main.py"
 try:
-    print(f"Running {file_name}...")
-    subprocess.run(["python", file_name], check=True)
-    print(f"Successfully ran {file_name}.")
+    print(f"Running {run_file}...")
+    subprocess.run(["python", run_file], check=True)
+    print(f"Successfully ran {run_file}.")
 except subprocess.CalledProcessError as e:
-    print(f"Failed to run {file_name}: {e}")
+    print(f"Failed to run {run_file}: {e}")
